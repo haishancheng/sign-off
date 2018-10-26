ab5f9b0bb181f4d62b8723e7f628524096576469
haishancheng <772142343@qq.com>
2018-10-26 20:37:13 +0800

    初次提交

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import moment from 'moment'
 
 class App extends Component {
+
+  state = {
+    timeList: [
+      {date: moment().format("YYYY.MM.DD"), time: moment().format("HH:mm:SS")},
+      {date: moment().format("YYYY.MM.DD"), time: moment().format("HH:mm:SS")},
+      {date: moment().format("YYYY.MM.DD"), time: moment().format("HH:mm:SS")},
+      {date: moment().format("YYYY.MM.DD"), time: moment().format("HH:mm:SS")}
+    ]
+  }
+
   render() {
+    let {timeList} = this.state
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="app">
+        <h1>下班平均时间计算系统</h1>
+        <p className="average-time">平均下班时间:<span>{moment().format("HH:mm:SS")}</span></p>
+        <ul>
+          {
+            timeList.map(item =>
+              <li>
+                <h2>日期:</h2>
+                <p className="date">{item.date}</p>
+                <h2>当天下班时间:</h2>
+                <p className="time">{item.time}</p>
+              </li>
+            )
+          }
+        </ul>
+        <button>签退</button>
       </div>
     );
   }
