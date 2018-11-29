d00fb2f3e2a3bc6ae52501ee64cb42384f90d24d
haishancheng <772142343@qq.com>
2018-11-29 17:51:26 +0800

    2018-11-29号细微改动

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
 import React, { Component } from 'react';
 import moment from 'moment'
-import { Modal } from 'antd';
+import { Modal, DatePicker   } from 'antd';
 import './App.css'
 const confirm = Modal.confirm;
+const { MonthPicker } = DatePicker
 
 class App extends Component {
 
   state = {
     timeList: [],
     hasSignOff: "",
-    currentDay: ""
+    currentDay: "",
+    month: moment().format("YYYY-MM")
+  }
+
+  componentWillMount() {
+    this.setState({
+      timeList: JSON.parse(localStorage.getItem("timeList") || "[]" ),
+      hasSignOff: localStorage.getItem("hasSignOff"),
+      currentDay: localStorage.getItem("currentDay")
+    })
   }
 
   signOff = () => {
     let {hasSignOff, currentDay} = this.state
-    if (hasSignOff === "yes" && moment().format("YYYY-MM-DD") === currentDay) {
-      Modal.info({
-        title: '当天已签退，如签退时间有误，请先删除！',
-      }); 
-      return
-    }
+    // if (hasSignOff === "yes" && moment().format("YYYY-MM-DD") === currentDay) {
+    //   Modal.info({
+    //     title: '当天已签退，如签退时间有误，请先删除！',
+    //   }); 
+    //   return
+    // }
     this.setState((prevState) => {
       let newTimeList = [...prevState.timeList, {time: moment().format('YYYY-MM-DD HH:mm:ss')}]
       localStorage.setItem("timeList", JSON.stringify(newTimeList))
@@ -59,16 +69,14 @@ class App extends Component {
     });
   }
 
-  componentWillMount() {
-    this.setState({
-      timeList: JSON.parse(localStorage.getItem("timeList") || "[]" ),
-      hasSignOff: localStorage.getItem("hasSignOff"),
-      currentDay: localStorage.getItem("currentDay")
-    })
+  changeMonth = (dateM, date) => {
+    console.log(dateM)
+    console.log(date)
+    this.setState({month: date})
   }
 
   render() {
-    let {timeList} = this.state
+    let {timeList, month} = this.state
     console.log('timeList', timeList)
     let totalHour = 0, totalMin = 0, totalSec = 0
     timeList.forEach((item) => {
@@ -88,11 +96,23 @@ class App extends Component {
     let timePointItem = timePoint.find((item) => {
       return moment(averageTime, "HH:mm:ss").valueOf() < moment(item.point, "HH:mm:ss").valueOf()
     })
-    let averageTimeClass = timePointItem ? timePointItem : "normal"
+    let averageTimeClass = timePointItem ? timePointItem.class : "normal"
+
     return (
       <div className="app">
         <h1>下班平均时间计算系统</h1>
-        <p className={`average-time ${averageTimeClass}`}>平均下班时间:<span>{averageTime === "NaN:NaN:NaN" ? "本月暂无统计" : averageTime}</span></p>
+        <p className={`average-time ${averageTimeClass}`}>
+          平均下班时间:
+          {
+            averageTime === "NaN:NaN:NaN" ?
+            <span style={{fontSize: "1em"}}>本月暂无统计</span> :
+            <span>{averageTime}</span>
+          }
+        </p>
+        <div className="month">
+          <span className="title">选择查看月份：</span>
+          <MonthPicker onChange={this.changeMonth} value={moment(month)} allowClear={false}/>
+        </div>
         <ul>
           {
             timeList.map((item, index) =>
@@ -107,6 +127,7 @@ class App extends Component {
           }
         </ul>
         <button onClick={this.signOff}>签退</button>
+        <footer>copyright {moment().year()} SeaMountCity</footer>
       </div>
     );
   }
